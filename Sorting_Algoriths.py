a642132700a3cc5bf70cc572bc00971491a80a3b
RodinaMohamed04 <es.rodaina.mohamed2023@alexu.edu.eg>
2026-03-12 07:19:24 +0200

    cont. bubble/insertion/selection sort and heap sort is added

---
+++ Sorting_Algoriths.py
@@ -0,0 +1,115 @@
+import random
+import time
+
+sizes = [5000,10000,25000]
+
+def generated_array(size):
+    arr = []
+    for i in range(size):
+        arr.append(random.randint(0,500000))
+
+    return arr
+
+#bubble sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        flag = False
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1]= arr[j+1],arr[j]
+                flag = True
+        if not flag :
+         break
+
+
+#selection sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        if min_index != i:
+         arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+#insertion sort
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        temp = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > temp:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = temp
+
+##### Assignment two
+
+#heap sort
+def Max_heap (arr,n,i):
+    largest = i
+    left = 2 * i +1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        Max_heap(arr,n,largest)
+def heap_sort(arr,n):
+    ##build max heap // O(n)
+    for i in range(n//2,-1,-1):
+        Max_heap(arr,n,i)
+    ##deletion // O(n log n)
+    for i in range(n-1,0,-1):
+        arr[i],arr[0] = arr[0],arr[i]
+        Max_heap(arr,n,0)
+
+
+for size in sizes:
+    arr = generated_array(size)
+    arr1 = arr.copy()
+    arr2 = arr.copy()
+    arr3 = arr.copy()
+    arr4 = arr.copy()
+
+    start = time.time()
+    bubble_sort(arr1)
+    end = time.time()
+    bubble_sort_time = (end - start)*1000
+
+
+    start = time.time()
+    selection_sort(arr2)
+    end = time.time()
+    selection_sort_time = (end - start) * 1000
+
+    start = time.time()
+    insertion_sort(arr3)
+    end = time.time()
+    insertion_sort_time = (end - start) * 1000
+
+### assignment two
+    start = time.time()
+    heap_sort(arr4,size)
+    end = time.time()
+    heap_sort_time = (end - start) * 1000
+
+
+
+    print("Array size :",size)
+    print("bubble sort time:",bubble_sort_time,"ms")
+    print("selection sort time:", selection_sort_time, "ms")
+    print("insertion sort time:", insertion_sort_time, "ms")
+    print("Heap sort time:", heap_sort_time, "ms")
+    print()
+
+
+
